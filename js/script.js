768563d3e5d6cf091d4c78a800189f0fb02523ce
Iuriy <webacademic@yandex.ru>
2021-11-18 13:31:26 +0300

    les1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    private: false
+};
+
+let questionOne = prompt('Один из просмотренных фильмов?', ''),
+    questionTwo = prompt('Его оценка?', ''),
+    questionThree = prompt('Один из просмотренных фильмов?', ''),
+    questionFour = prompt('Его оценка?', '');
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+
+console.log(personalMovieDB123123);
